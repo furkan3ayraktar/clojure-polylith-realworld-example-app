91569929be9df01740001f644b5e62091f7a3849
Furkan Bayraktar <me@furkanbayraktar.com>
2018-04-01 13:16:50 +0200

    Fixed dependencies for tags component.

--- components/tags/project.clj
+++ components/tags/project.clj
@@ -1,5 +1,9 @@
 (defproject clojure.org.realworld/tags "0.1"
   :description "A tags component"
   :dependencies [[clojure.org.realworld/interfaces "1.0"]
-                 [org.clojure/clojure "1.9.0"]]
+                 [java-jdbc/dsl "0.1.3"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.clojure/test.check "0.9.0"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]]
   :aot :all)
