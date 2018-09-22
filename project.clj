9075f6d8e31de626d3095358ca36c7c769e1ebf1
Furkan Bayraktar <me@furkanbayraktar.com>
2018-09-22 18:45:50 +0200

    Increased polylith version.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure.realworld/realworld "1.0"
   :description "The workspace"
-  :plugins [[polylith/lein-polylith "0.0.46-alpha"]]
+  :plugins [[polylith/lein-polylith "0.0.48-alpha"]]
   :polylith {:top-namespace   "clojure.realworld"
              :clojure-version "1.9.0"})
