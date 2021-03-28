e1b7307a0368d9f0b12801360d77b4fbd871e864
Peter Strömberg <pez@pezius.com>
2021-03-28 16:35:37 +0200

    Highlight using REPL in documentation (#15)
    * Make it work out of the box with RW frontends
    
    * Add Calva REPL start settings
    
    * Add server start/stop commands
    
    * Add development server start/stop
    
    * Focus the start of the README on REPL dev
    
    * Info regarding starting w/ dev and test aliases
    
    * Refer to workspace  info
    
    * Fix TOC link to poly check
    
    * Sorted ns form

---
+++ development/src/dev/server.clj
@@ -0,0 +1,12 @@
+(ns dev.server
+  (:require [clojure.realworld.rest-api.main :as main]))
+
+(defn start! [port]
+  (main/start! port))
+
+(defn stop! []
+  (main/stop!))
+
+(comment
+  (start! 6003)
+  (stop!))
