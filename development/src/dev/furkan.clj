bb85e8f4b4dd02f958e369c77c3746578a32e7f2
Furkan Bayraktar <me@furkanbayraktar.com>
2023-02-26 14:22:10 +0100

    Add djblue/portal to development project

--- development/src/dev/furkan.clj
+++ development/src/dev/furkan.clj
@@ -1,1 +1,15 @@
-(ns dev.furkan)
+(ns dev.furkan
+  (:require [portal.api :as p]))
+
+(def portal-atom (atom nil))
+
+(defn launch-portal []
+  (let [portal (p/open (or @portal-atom {:launcher :vs-code}))]
+    (reset! portal-atom portal)
+    (add-tap #'p/submit)
+    portal))
+
+(comment
+  (launch-portal)
+
+  )
