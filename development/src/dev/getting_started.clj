acb14836830054a2e8ef8a7c6fb60e3ade04f63b
Peter Strömberg <pez@pezius.com>
2023-10-24 14:19:30 +0200

    Add RCF Fetch articles

--- development/src/dev/getting_started.clj
+++ development/src/dev/getting_started.clj
@@ -1,4 +1,5 @@
-(ns dev.getting-started)
+(ns dev.getting-started
+  (:require [clj-http.client :as http]))
 
 ;; Welcome to the REPL for Getting Started with Polylith Real World example!
 ;;
@@ -13,4 +14,17 @@
 "Hello Polylith Real World!"
 
 ;; Great! Let's continue.
+
+(comment
+  (def res (http/get "http://localhost:6003/api/articles" {:as :auto}))
+  (:body res)
+
+  ;; TODO: add an article via the API
+  ;;       will we need to create a user first?
+  :rcf)
+
+
+
+
 ;; TODO: @furkan3ayraktar and @tengstrand and @Misophistful, please fill in the blank space!
+
