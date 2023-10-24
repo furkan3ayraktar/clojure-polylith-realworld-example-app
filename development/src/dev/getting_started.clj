006a961aca0d14d349415c79db8075462f9e6b1a
Peter Strömberg <pez@pezius.com>
2023-10-24 19:02:57 +0200

    Add some action in getting_started

--- development/src/dev/getting_started.clj
+++ development/src/dev/getting_started.clj
@@ -4,27 +4,78 @@
 ;; Welcome to the REPL for Getting Started with Polylith Real World example!
 ;;
 ;; If you are unfamiliar with Calva or Clojure, please open the file
-;; development/src/dev/getting_started.clj
-;; Cmd/Control-clicking this symbol will do it:
-'dev.hello-repl
+;; development/src/dev/hello_repl.clj
 
 ;; To confirm that you know Calva well enough to continue this session,
-;; Evaluate this string:
+;; 1. Load this file in the REPL
+;; 2. Evaluate this string:
 
 "Hello Polylith Real World!"
 
 ;; Great! Let's continue.
+;; The guide is designed to run in Rich Comment Forms like the one below
+;; You are supposed to read instructions and evaluate the top level forms
+;; in the RCF (in that order 😀). If there is no instruction to evaluate
+;; a top level form, consider it an implied instruction to do so.
+;; Please don't hesitate to experiment with the code!
 
 (comment
-  (def res (http/get "http://localhost:6003/api/articles" {:as :auto}))
-  (:body res)
+  ;; The server is running, we should be able to grab some articles
 
-  ;; TODO: add an article via the API
-  ;;       will we need to create a user first?
+  ;; This is the API base URL
+  (def base-url "http://localhost:6003/api")
+
+  ;; Fetch some articles
+  (def articles (http/get (str base-url "/articles") {:as :auto}))
+  ;; Look at them
+  (:body articles)
+
+  ;; Oh, no articles? We need to add one!
+
+  ;; First we need to create a user, this payload.
+  (def register-payload {:user {:username "Polylith Explorer"
+                                :email "polylith-is-cool@example.com"
+                                :password "battery-staple-horse"}})
+
+  ;; Creating the user
+  ;; We hold on to the token so that we can create articles later
+  (def jwt-token (let [register-response (http/post (str base-url "/users")
+                                                    {:form-params register-payload
+                                                     :content-type :json
+                                                     :as :auto})]
+                   (->> register-response :body :user :token)))
+  ;; Consider evaluating `jwt-token` above to “save” the token in the
+  ;; repl/output window.
+
+  ;; Now our first article
+  (def article-payload {:article {:title "Polylith Real World example article"
+                                  :description "My first article. And it is about Polylith of course!"
+                                  :body "Yada, yada, yada. Too lazy right now"
+                                  :tagList ["polylith" "clojure" "realworld"]}})
+
+  (def article-request-headers {"Authorization" (str "Token " jwt-token)})
+
+  (http/post (str base-url "/articles")
+             {:form-params article-payload
+              :headers article-request-headers
+              :content-type :json})
+
+  ;; We define the request so that we can examine it later
+  (def article-request *1)
+
+
+  (def articles-again (http/get (str base-url "/articles") {:as :auto}))
+  (:body articles-again)
+  (->> articles-again :body count)
+
+  ;; TODO: @furkan3ayraktar and @tengstrand and @Misophistful, please fill in the blank space!
+
+
+
+
+
   :rcf)
 
 
 
-
-;; TODO: @furkan3ayraktar and @tengstrand and @Misophistful, please fill in the blank space!
-
+"Hello dear Polylith Real World explorer! You loaded this file in the REPL."
